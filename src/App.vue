46382a807fffc2204627eb844fc07621a89cb034
Vedat Demiri <vedatdemiri@osx.home>
2021-01-09 23:17:18 +0100

    WIP: finished first component on the introduction page, login component is next

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import Navigation from "@/components/Navigation";
 import Introduction from "@/components/Introduction";
+
 export default {
   components: { Introduction, Navigation },
   created() {}
@@ -16,4 +17,13 @@ export default {
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Montserrat&family=Teko:wght@400;600&display=swap");
+
+body {
+  font-family: "Montserrat", sans-serif !important;
+}
+
+h1 {
+  font-family: "Teko", sans-serif !important;
+  text-transform: uppercase;
+}
 </style>
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="looongboi"></div>
+  <div class="container vh-100">
+    <div class="row pt-5">
+      <div class="col-sm d-flex justify-content-center pt-5">
+        <IndexPresentation />
+      </div>
+      <div class="col-sm d-flex justify-content-center pt-5">
+        One of three columns
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import IndexPresentation from "@/components/IndexPresentation";
 export default {
-  name: "Introduction.vue"
+  name: "Introduction.vue",
+  components: { IndexPresentation },
+  data: function() {
+    return {
+      scrollPosition: null,
+      isResponsive: true,
+      windowWidth: window.innerWidth
+    };
+  }
 };
 </script>
 
-<style scoped>
-.looongboi {
-  height: 499999px;
-}
-</style>
+<style scoped></style>
